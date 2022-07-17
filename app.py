d87d7a52a82b610f4055739733694798439e64a3
Angie Yeh <angieyeh24@gmail.com>
2022-07-17 00:30:53 -0700

    Setup Flask server with example jinja template

---
+++ app.py
@@ -0,0 +1,25 @@
+# Citation for the following phone numbers:
+# Date: 07/17/2022
+# Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app
+
+from flask import Flask, render_template
+import os
+
+# Configuration
+
+app = Flask(__name__)
+
+# Routes 
+
+@app.route('/')
+def root():
+  return render_template("main.j2")
+
+# Listener
+
+if __name__ == "__main__":
+  port = int(os.environ.get('PORT', 8080)) 
+  #                                 ^^^^
+  #              You can replace this number with any valid port
+  
+  app.run(port=port, debug=True) 
