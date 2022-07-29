e3fa2466e0b8e28a6e50baf2a45360167d522e4e
Bema Fisher <fishebeg@oregonstate.edu>
2022-07-28 19:20:30 -0700

    removed extra code

--- app_config.py
+++ app_config.py
@@ -10,16 +10,10 @@ app = Flask(__name__)
 
 # Connect to the database
 connection = pymysql.connect(host='classmysql.engr.oregonstate.edu',
-                             user='cs340_fishebeg',
-                             password='7237',
-                             database='cs340_fishebeg',
+                             user='cs340_onid',
+                             password='XXXX',
+                             database='cs340_onid',
                              cursorclass=pymysql.cursors.DictCursor)
-
-# app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
-# app.config['MYSQL_USER'] =  os.environ.get('MYSQL_USER')
-# app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD') 
-# app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
-# app.config['MYSQL_CURSORCLASS'] = os.environ.get('MYSQL_CURSORCLASS')
 
 def application():
   return app
