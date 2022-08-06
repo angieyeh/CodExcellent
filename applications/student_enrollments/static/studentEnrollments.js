f6f5f5600120ec0fc9b6e435ab0dca1b26590e41
Angie Yeh <angieyeh24@gmail.com>
2022-08-06 00:00:21 -0700

    Update schema with sample data and CASCADE for certificates

--- applications/student_enrollments/static/studentEnrollments.js
+++ applications/student_enrollments/static/studentEnrollments.js
@@ -13,36 +13,36 @@ const deleteStudentEnrollment = async (studentEnrollmentId) => {
   // Date: 08/01/2022
   // Adapted from 
   // Source URL: https://stackoverflow.com/questions/21756777/jquery-find-element-by-data-attribute-value 
-  $(`#browse tr[data-student-enrollment-id="${studentEnrollmentId}"]`).remove();
+  $(`#browse-se tr[data-student-enrollment-id="${studentEnrollmentId}"]`).remove();
 }
 
 
-const wireTableDeleteButtons = () => {
+const wireSETableDeleteButton = () => {
   // Citation for opening and closing the modal
   // Date: 08/01/2022
   // Copied from 
   // Source URL: https://stackoverflow.com/questions/33650266/bootstrap-modal-open-button-requires-two-clicks-to-reopen-after-closing 
-  $("#browse").on("click", ".btn-modal-delete", function() {
+  $("#browse-se").on("click", ".btn-modal-delete", function() {
     $(".modal").modal('toggle');
     $(".modal").data('student-enrollment-id', $(this).data('student-enrollment-id'));
     $(".modal-title").text('Delete Student Enrollment');
     $(".modal-body").text(`Are you sure you want to delete Student Enrollment ID ${$(this).data('student-enrollment-id')}?`);
-    $(".modal-footer .btn-primary").addClass('btn-delete');
+    $(".modal-footer .btn-primary").addClass('btn-delete-se');
     $(".modal-footer .btn-primary").text('Delete');
-    $(".btn-delete").prop('disabled', false);
+    $(".btn-delete-se").prop('disabled', false);
   });
 }
 
 
-const wireDeleteButton = () => {
-  $(".modal").on("click", '.btn-delete', function() {
+const wireSEDeleteButton = () => {
+  $(".modal").on("click", '.btn-delete-se', function() {
     let studentEnrollmentId = $(".modal").data('student-enrollment-id')
     $(this).prop('disabled', true);
     deleteStudentEnrollment(studentEnrollmentId);
   });
 }
 
-const wireTableUpdateButtons = () => {
+const wireSETableUpdateButton = () => {
   $("#browse").on("click", ".btn-update", function() {
     let studentEnrollmentId = $(this).data('student-enrollment-id')
     window.location.replace(`/student_enrollments/${studentEnrollmentId}`);
@@ -51,7 +51,7 @@ const wireTableUpdateButtons = () => {
 
 
 $(document).ready(function() {
-  wireTableDeleteButtons();
-  wireDeleteButton();
-  wireTableUpdateButtons();
+  wireSETableDeleteButton();
+  wireSEDeleteButton();
+  wireSETableUpdateButton();
 })
